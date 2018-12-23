497dd4e16e792c9ba22a36656383b602416b90bb
heniek <heiovsky@gmail.com>
2018-12-23 18:37:52 +0100

    init

---
+++ src/main/java/phwb/ApplicationRoot.java
@@ -0,0 +1,11 @@
+package phwb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRoot {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRoot.class, args);
+    }
+}
---
+++ src/main/java/phwb/sensors/SensorEndpointController.java
@@ -0,0 +1,26 @@
+package phwb.sensors;
+
+import com.profesorfalken.jsensors.JSensors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SensorEndpointController {
+
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value = "/sensors", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public synchronized ResponseEntity index() {
+
+        try {
+            return new ResponseEntity(JSensors.get.components(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity("Error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
+
